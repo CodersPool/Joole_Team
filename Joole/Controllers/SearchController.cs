b6ae63e43f8124d1477bc014950946e5b4d9fc6b
Lily <lilytang479@gmail.com>
2019-05-14 21:51:26 -0400

    SearchPage Update

---
+++ Joole/Controllers/SearchController.cs
@@ -0,0 +1,46 @@
+﻿using Joole.Models;
+using JooleService;
+using JooleService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Joole.Controllers
+{
+    public class SearchController : Controller
+    {
+        Service service = new Service();
+        // GET: Search
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+
+        public ActionResult ShowCategories()
+        {
+            return Json(service.ShowAllCategories(), JsonRequestBehavior.AllowGet);
+        }
+
+        //public JsonResult ShowCategories()
+        //{
+        //    var x = service.ShowAllCategories();
+        //    return Json(x, JsonRequestBehavior.AllowGet);
+        //}
+
+        [HttpPost]
+        public ActionResult AutoCompleteSubCategory(SubCategoryViewModel model)
+        {
+            return Json(service.AutoCompleteSubCategories(model.name, model.category_Id));
+        }
+        //[HttpPost]
+        //public JsonResult AutoCompleteSubCategory(SubCategoryViewModel model)
+        //{
+        //    var x = service.AutoCompleteSubCategories(model.name, model.category_Id);
+        //    return Json(service.AutoCompleteSubCategories(model.name, model.category_Id));
+        //}
+
+
+    }
+}
---
+++ JooleService/Models/SubCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JooleService.Models
+{
+    public class SubCategory
+    {
+        public int id { get; set; }
+        public int categoryId { get; set; }
+
+        public string name { get; set; }
+
+        public SubCategory(int id, int? categoryId, string name)
+        {
+            this.id = id;
+            this.categoryId = categoryId ?? default(int);
+            this.name = name;
+        }
+    }
+}
